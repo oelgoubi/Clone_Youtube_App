3180081fb35b9f928d92c5a058f8755c9996393b
oelgoubi <othmanelgoubi@gmail.com>
2020-05-26 01:44:14 +0200

    Updates

---
+++ precache-manifest.0775cdf45cbcdd7fc8d699a09dd0ffc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b762c01b950de7abf7889e92480e0b",
+    "url": "/Clone_Youtube_App/index.html"
+  },
+  {
+    "revision": "3de87412b3039e945f95",
+    "url": "/Clone_Youtube_App/static/css/main.3586ebb7.chunk.css"
+  },
+  {
+    "revision": "46835a5d9425aefb3d3f",
+    "url": "/Clone_Youtube_App/static/js/2.42261578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clone_Youtube_App/static/js/2.42261578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de87412b3039e945f95",
+    "url": "/Clone_Youtube_App/static/js/main.9d75ae2e.chunk.js"
+  },
+  {
+    "revision": "75b0eb0a27184683f41c",
+    "url": "/Clone_Youtube_App/static/js/runtime-main.711cbafe.js"
+  }
+]);
